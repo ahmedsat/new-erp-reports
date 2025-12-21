4cf649ee031a6f99f4f745f5cebba7d5a184a65d
ahmedsat <ahmedsat303@gmail.com>
2025-12-21 13:28:36 +0200

    update slice

--- utils/slices.go
+++ utils/slices.go
@@ -9,3 +9,25 @@ func FindF[T any](slice []T, predicate func(T) bool) T {
 	var zero T
 	return zero
 }
+
+func UpdateF[T any](slice []T, predicate func(T) bool, updater func(T) T) []T {
+	for i, item := range slice {
+		if predicate(item) {
+			slice[i] = updater(item)
+		}
+	}
+	return slice
+}
+
+func TruePredicate[T any]() func(T) bool {
+	return func(T) bool { return true }
+}
+
+func FindIndexF[T any](slice []T, predicate func(T) bool) int {
+	for i, item := range slice {
+		if predicate(item) {
+			return i
+		}
+	}
+	return -1
+}
